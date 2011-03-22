cc38fa1931c7679a0122751482d250a99bf4d1e8
Andrew Brader <abrader@itmat.upenn.edu>
2011-03-22 17:37:11 -0400

    More work on AWS spot request into bootstrapping but not quite there.

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,6 +1,7 @@
 class Node < ActiveRecord::Base
   
   require 'chef/knife/bootstrap'
+  require 'chef/knife/ssh'
   
   @@connection = Fog::Compute.new(
     :provider => 'AWS',
@@ -10,7 +11,14 @@ class Node < ActiveRecord::Base
   
   @@ec2 = RightAws::Ec2.new(Chef::Config[:knife][:aws_access_key_id], Chef::Config[:knife][:aws_secret_access_key])
   
-  
+  def after_initialize()
+    @instance_id = nil
+    @sir_state = nil
+    @aws_state = nil
+    @hostname = nil
+    @instance_id = nil
+    @spot_instance_request_id = nil
+  end
   
   def chef_server_rest
     Chef::REST.new(Chef::Config[:chef_server_url])
@@ -116,12 +124,11 @@ class Node < ActiveRecord::Base
     #instance_bootstrap(instance).run
   end
   
-  def self.start_by_spot_request
+  def start_by_spot_request
     # This is the AWS reduced cost spot request
     require 'net/ssh/multi'
     
     instance = nil
-    instance_id = nil
     
     sir = @@ec2.request_spot_instances(
             :image_id => Chef::Config[:knife][:image],
@@ -133,29 +140,37 @@ class Node < ActiveRecord::Base
             :instance_type => Chef::Config[:knife][:flavor]
     )
     
-    spot_instance_request_id = sir[0][:spot_instance_request_id]
+    @spot_instance_request_id = sir[0][:spot_instance_request_id]
     
-    while instance_id == nil
+    # Must hang in a loop until Amazon issues us a instance id
+    while @instance_id == nil
       sleep(5)
-      status = Node.describe_spot_instance_request(spot_instance_request_id) 
-      instance_id = status[:instance_id]
-      state = status[:state]
-      break if (state == nil || state == "cancelled" || state == "failed")
+      status = Node.describe_spot_instance_request(@spot_instance_request_id) 
+      @instance_id = status[:instance_id]
+      @sir_state = status[:state]
+      break if (@sir_state == nil || @sir_state == "cancelled" || @sir_state == "failed")
     end
     
-    @@ec2.cancel_spot_instance_requests(spot_instance_request_id)
+    # Get the status of our instance now that we have an instance ID
+    self.get_instance_status
     
-    puts "This should be your instance id: #{instance_id}. It has a class of #{instance_id.class}"
+    # Once we're done with the spot instance request we can cancel it
+    @@ec2.cancel_spot_instance_requests(@spot_instance_request_id)
     
-    # FIXME The variable instance is not getting set for whatever reason.
-    instance = Node.describe_instance(instance_id)
+    # Must wait for aws_state to go from Pending to Active in order to get pertinent host information i.e., hostname
+    while @aws_state =~ /pending/
+      sleep(5)
+      self.get_instance_status
+    end
     
-    puts instance[:dns_name]
-    hostname = instance[:dns_name]
+    # Get the latest info on our instance now that hostname should be populated
+    self.get_instance_status
     
     # wait for it to be ready to do stuff
-    Node.wait_for_ssh(hostname)
+    Node.wait_for_ssh(@hostname)
 
+    # FIXME Something is getting hung up when Chef libs attempt to contact compute node via SSH
+    # Time to get that instance boostrapped with Chef-client
     instance_bootstrap(instance).run
   end
   
@@ -163,8 +178,15 @@ class Node < ActiveRecord::Base
     @@ec2.describe_spot_instance_requests(spot_request_id)[0]
   end
   
-  def self.describe_instance(instance_id)
-    @@ec2.describe_instances(instance_id)[0]
+  # Sets @aws_state, @hostname
+  def get_instance_status()
+    if @instance_id == nil
+      return false
+    else
+      instance = @@ec2.describe_instances(@instance_id)[0]
+      @aws_state = instance[:aws_state]
+      @hostname = instance[:dns_name]
+    end    
   end
   
   def self.wait_for_ssh(host)
@@ -191,26 +213,27 @@ class Node < ActiveRecord::Base
     return true
   end
   
-  def self.instance_bootstrap(instance)
+  def instance_bootstrap(instance)
     bootstrap = Chef::Knife::Bootstrap.new
-    bootstrap.name_args = [instance.dns_name] || instance[:dns_name]
+    bootstrap.name_args = @hostname
     # TODO @name_args is argv on the command line for Knife, need to replace with proper runlist.
     # Comma separated list of roles/recipes to apply
     bootstrap.config[:run_list] = @name_args
-    bootstrap.config[:ssh_user] = config[:ssh_user] || Chef::Config[:knife][:ssh_user]
+    bootstrap.config[:ssh_user] = Chef::Config[:knife][:ssh_user]
     # The SSH identity file used for authentication
-    bootstrap.config[:identity_file] = config[:identity_file] || Chef::Config[:knife][:ssh_client_key]
+    bootstrap.config[:identity_file] = Chef::Config[:knife][:ssh_client_key]
     # The Chef node name for your new node
-    bootstrap.config[:chef_node_name] = config[:chef_node_name] || instance.id || instance[:instance_id]
+    bootstrap.config[:chef_node_name] = @instance_id
     # This is if you want pre-release Chef client gems to be installed
-    bootstrap.config[:prerelease] = config[:prerelease]
+    #bootstrap.config[:prerelease] = config[:prerelease]
     # Bootstrap a distro using a template
-    bootstrap.config[:distro] = config[:distro]
+    #bootstrap.config[:distro] = config[:distro]
     bootstrap.config[:use_sudo] = true
     # Full path to location of template to use
-    bootstrap.config[:template_file] = config[:template_file]
+    #bootstrap.config[:template_file] = config[:template_file]
     # Not sure environment is even used since I couldn't find it referenced in › chef› lib› chef› knife› bootstrap.rb
-    bootstrap.config[:environment] = config[:environment]
+    #bootstrap.config[:environment] = config[:environment]
+    bootstrap
   end
   
   def self.reconcile_nodes()
